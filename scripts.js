0df9fd2d8197cd67505502704e6c12c75dd300d6
Kap_God <kavinprasath2000@gmail.com>
2025-03-21 21:59:20 +0530

    Commit

--- scripts.js
+++ scripts.js
@@ -5,23 +5,23 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const particlesArray = [];
-const numberOfParticles = 100;
+const numberOfParticles = 50; // Reduced for subtlety
 
 class Particle {
     constructor() {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1;
-        this.speedX = Math.random() * 3 - 1.5;
-        this.speedY = Math.random() * 3 - 1.5;
+        this.size = Math.random() * 3 + 1;
+        this.speedX = Math.random() * 1 - 0.5;
+        this.speedY = Math.random() * 1 - 0.5;
     }
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
-        if (this.size > 0.2) this.size -= 0.1;
+        if (this.size > 0.1) this.size -= 0.05;
     }
     draw() {
-        ctx.fillStyle = 'rgba(0, 255, 204, 0.8)';
+        ctx.fillStyle = 'rgba(0, 204, 153, 0.5)';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();
@@ -39,7 +39,7 @@ function animate() {
     for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
         particlesArray[i].draw();
-        if (particlesArray[i].size <= 0.2) {
+        if (particlesArray[i].size <= 0.1) {
             particlesArray.splice(i, 1);
             i--;
             particlesArray.push(new Particle());
